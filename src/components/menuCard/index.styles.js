49b1455ef155bdd10c1e007b646b45af0cbc1ae9
Thilina Warnakula <thilina9292.contact@gmail.com>
2022-02-14 23:45:36 +0530

    ui changes

--- src/components/menuCard/index.styles.js
+++ src/components/menuCard/index.styles.js
@@ -12,20 +12,18 @@ const cardHeight = 150;
 const style = EStyleSheet.create({
     cardGrid: {
         borderRadius: '15rem',
-        backgroundColor: COLORS.gray2,
         marginEnd: 40,
         width : 200
     },
     cardList:{
         height: cardHeight,
-        backgroundColor: COLORS.gray2,
         marginTop:'30rem',
         marginBottom:'50rem',
         borderRadius: '15rem',
     },
     image: {
         width: '100%',
-        height: cardHeight,
+        height: cardHeight - 20,
         borderRadius: '15rem',
         alignItems: 'center'
     },
