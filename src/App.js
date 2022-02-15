09e13c9400e8450c9919e4bef5f41405d574120f
Thilina Warnakula <thilina9292.contact@gmail.com>
2022-02-15 08:01:46 +0530

    adding redux persist and persisit rating data

--- src/App.js
+++ src/App.js
@@ -7,22 +7,21 @@ import {
 } from 'react-native';
 
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import ReduxThunk from 'redux-thunk';
-import reducers from '../src/redux/reducers';
+import { PersistGate } from 'redux-persist/integration/react';
 
+import { store, persistor } from './redux/reducers';
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
     };
-    this.store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
   }
 
   render() {
     LogBox.ignoreAllLogs();
     return (
-      <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}></PersistGate>
         <View style={{ flex: 1 }}>
           <RootNavigator />
         </View>
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,20 @@
-import {combineReducers} from 'redux';
+
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { persistStore, persistReducer } from 'redux-persist';
+
 import MovieReducer from './movieReducer';
 
-export default combineReducers({
-  movies: MovieReducer,
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  blacklist : [],
+};
+
+const rootReducer = combineReducers({
+  movies: persistReducer(persistConfig, MovieReducer)
 });
+
+export const store = createStore(rootReducer, applyMiddleware(thunk));
+export const persistor = persistStore(store);
